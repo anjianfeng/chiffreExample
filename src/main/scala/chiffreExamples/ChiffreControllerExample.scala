02571526ff24bc4e1c5444db2bc7104177dd90b7
Jianfeng An <anjf@nwpu.edu.cn>
2019-04-27 11:22:19 +0800

    Update ChiffreControllerExample.scala

--- src/main/scala/chiffreExamples/ChiffreControllerExample.scala
+++ src/main/scala/chiffreExamples/ChiffreControllerExample.scala
@@ -2,20 +2,20 @@
 package examples
 
 import chisel3._
+
 import chiffre._
-import chiffre.passes.{ScanChainAnnotation, ScanChainDescriptionAnnotation, FaultInjectionAnnotation}
-import chiffre.inject.{IdentityInjector, Injector, LfsrInjector32, StuckAt, CycleInjector32, StuckAtInjectorWithId}
+import chiffre.passes._
+import chiffre.inject._
 
 import scala.language.reflectiveCalls
 
 class ChiffreControllerExample(val scanId: String) extends Module with ChiffreController {
   val io = IO(new Bundle{})
   val clk = Reg(UInt(1.W))
+  // Scan enable every clock
   clk := ~clk
   scan.clk := clk
   scan.en := ~clk
-  //scan.out := clk
+  scan.out := clk
   //assert(scan.in === scan.out)
 }
-
-
