8da9d582ebc6e3c56e215500bd7339340b4d618b
Jianfeng An <anjf@nwpu.edu.cn>
2019-04-27 11:24:11 +0800

    Update and rename ChiffreExampleTest.scala to ShiftRegisterTests.scala

--- src/test/scala/chiffreExamplesTests/ShiftRegisterTests.scala
+++ src/test/scala/chiffreExamplesTests/ShiftRegisterTests.scala
@@ -2,14 +2,11 @@
 package examples
 
 import chisel3._
-import chisel3.iotesters.ChiselFlatSpec
-import chiffre.{InjectorInfo, ChiffreController, ChiffreInjector, ChiffreInjectee}
-import chiffre.passes.{ScanChainAnnotation, ScanChainDescriptionAnnotation, FaultInjectionAnnotation}
-import chiffre.inject.{Injector, LfsrInjector32}
-
-import chisel3.core.Bits
-import chisel3.{iotesters}
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+
+import chiffre._
+import chiffre.passes._
+import chiffre.inject._
 
 import scala.language.reflectiveCalls
 
@@ -26,31 +23,12 @@ class ShiftRegisterTests(c: ShiftRegister) extends PeekPokeTester(c) {
   }
 }
 
-
-
-
 class ShiftRegisterTester extends ChiselFlatSpec {
 
   behavior of "ShiftRegister"
   backends foreach {backend =>
     it should s"shift a number through a series of registers in $backend" in {
       Driver(() => new ShiftRegister, backend)((c) => new ShiftRegisterTests(c)) should be (true)
-    }
-  }
-}
-
-class MyCounterTests(c: MyCounter)extends PeekPokeTester(c) {
-  poke(c.io.in, 0.U)
-  step(4)  
-  expect(c.io.out, 4.U)
-}
-
-class MyCounterTester extends ChiselFlatSpec {
-
-  behavior of "MyCounter"
-  backends foreach {backend =>
-    it should s"increase counter in $backend" in {
-      Driver(() => new MyCounter, backend)((c) => new MyCounterTests(c)) should be (true)
     }
   }
 }
